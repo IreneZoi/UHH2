46c289104ecf8fd305c2fac4bdd926018f293d33
Irene <irene.zoi@cern.ch>
2018-12-11 14:11:44 +0100

    create xml for 2016 presel

---
+++ scripts/createXML.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_HT300to500*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_HT500to700*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_H700to1000*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_HT1000to1500*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_HT1500to2000*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_QCD_PUPPI/uhh2.AnalysisModuleRunner.MC.MC_QCD_HT2000toInf*'
+
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016B*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016C*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016D*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016E*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016F*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016G*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016H_ver2*'
+#python create-multi-dataset-xmlfiles.py '/nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/workdir_data_JetHT_PUPPI/uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016H_ver3*'
+
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT300to500.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT500to700.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT700to1000.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT1000to1500.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT1500to2000.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.MC.MC_QCD_HT2000toInf.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016B.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016C.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016D.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016E.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016F.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016G.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016H_ver2.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
+cp uhh2.AnalysisModuleRunner.Data.DATA_JetHT_Run2016H_ver3.xml /nfs/dust/cms/user/zoiirene/DiBoson/OutputBatch/PreSelection/xmlfiles/
