1d72a2439952b652625cc2ca7b27308772b826ec
Irene <irene.zoi@cern.ch>
2019-05-15 11:12:12 +0200

    updated puppi v4 ntuplewriter with extra_trigger_bits

--- core/python/ntuplewriter_mc_2016v2_puppi.py
+++ core/python/ntuplewriter_mc_2016v2_puppi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-from UHH2.core.ntuple_generator_puppi import generate_process  # use CMSSW type path for CRAB
+from UHH2.core.ntuple_generator_puppi_new import generate_process  # use CMSSW type path for CRAB
 from UHH2.core.optionsParse import setup_opts, parse_apply_opts
 
 
